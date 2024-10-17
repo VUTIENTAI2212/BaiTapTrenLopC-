2886238c8ee3fe1fc96fe7647d93e63c9f0d325d
VUTIENTAI2212 <vutai80963697@gmail.com>
2024-10-17 16:23:03 +0700

    1
    1

---
+++ TrangDangNhap/TrangDangNhap/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace TrangDangNhap
+{
+    partial class TrangDangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.textBox1_Email = new System.Windows.Forms.TextBox();
+            this.textBox2_Password = new System.Windows.Forms.TextBox();
+            this.button1_DangKy = new System.Windows.Forms.Button();
+            this.Username_texBox = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(329, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG KÝ";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.Location = new System.Drawing.Point(53, 206);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(88, 36);
+            this.Email.TabIndex = 1;
+            this.Email.Text = "Email";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(53, 273);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(147, 36);
+            this.Password.TabIndex = 2;
+            this.Password.Text = "Password";
+            // 
+            // textBox1_Email
+            // 
+            this.textBox1_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_Email.Location = new System.Drawing.Point(244, 203);
+            this.textBox1_Email.Multiline = true;
+            this.textBox1_Email.Name = "textBox1_Email";
+            this.textBox1_Email.Size = new System.Drawing.Size(508, 39);
+            this.textBox1_Email.TabIndex = 3;
+            this.textBox1_Email.TextChanged += new System.EventHandler(this.textBox1_Email_TextChanged);
+            // 
+            // textBox2_Password
+            // 
+            this.textBox2_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2_Password.Location = new System.Drawing.Point(244, 270);
+            this.textBox2_Password.Multiline = true;
+            this.textBox2_Password.Name = "textBox2_Password";
+            this.textBox2_Password.Size = new System.Drawing.Size(508, 39);
+            this.textBox2_Password.TabIndex = 4;
+            // 
+            // button1_DangKy
+            // 
+            this.button1_DangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_DangKy.Location = new System.Drawing.Point(271, 352);
+            this.button1_DangKy.Name = "button1_DangKy";
+            this.button1_DangKy.Size = new System.Drawing.Size(267, 51);
+            this.button1_DangKy.TabIndex = 5;
+            this.button1_DangKy.Text = "Đăng Ký";
+            this.button1_DangKy.UseVisualStyleBackColor = true;
+            this.button1_DangKy.Click += new System.EventHandler(this.button1_DangKy_Click);
+            // 
+            // Username_texBox
+            // 
+            this.Username_texBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username_texBox.Location = new System.Drawing.Point(244, 144);
+            this.Username_texBox.Multiline = true;
+            this.Username_texBox.Name = "Username_texBox";
+            this.Username_texBox.Size = new System.Drawing.Size(508, 39);
+            this.Username_texBox.TabIndex = 7;
+            this.Username_texBox.TextChanged += new System.EventHandler(this.Username_texBox_TextChanged);
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(53, 147);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(150, 36);
+            this.Username.TabIndex = 6;
+            this.Username.Text = "Username";
+            // 
+            // TrangDangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(819, 460);
+            this.Controls.Add(this.Username_texBox);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.button1_DangKy);
+            this.Controls.Add(this.textBox2_Password);
+            this.Controls.Add(this.textBox1_Email);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.label1);
+            this.Name = "TrangDangKy";
+            this.Text = "Trang Đăng Ký";
+            this.Load += new System.EventHandler(this.TrangDangKy_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox textBox1_Email;
+        private System.Windows.Forms.TextBox textBox2_Password;
+        private System.Windows.Forms.Button button1_DangKy;
+        private System.Windows.Forms.TextBox Username_texBox;
+        private System.Windows.Forms.Label Username;
+    }
+}
+
---
+++ TrangDangNhap/TrangDangNhap/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrangDangNhap
+{
+    public partial class TrangDangKy : Form
+    {
+        public TrangDangKy()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Email_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_DangKy_Click(object sender, EventArgs e)
+        {
+            String email = textBox1_Email.Text;
+            String password = textBox2_Password.Text;
+            String patternPassword = @"^(?=.*[!@#$%^&*(),.?""{}|<>])[A-Za-z\d!@#$%^&*(),.?""{}|<>]{8,}$";
+            string patternEmail = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            // Kiểm tra mật khẩu với regex
+            
+            if (Username_texBox.Text == "" || Regex.IsMatch(email, patternEmail) ==false || Regex.IsMatch(password, patternPassword) == false )
+            {
+                // Kiểm tra tên
+                if (Username_texBox.Text == "")
+                {
+                    MessageBox.Show("Username không được bỏ trống.");
+                    return;
+                }
+                // Kiểm tra email với regex
+                else if (Regex.IsMatch(email, patternEmail) == false)                               
+                {
+                    MessageBox.Show("Email yêu cầu nhập đúng định dạng.");
+                }
+                else
+                {
+                    MessageBox.Show("Mật khẩu không hợp lệ. Yêu cầu tối thiểu 8 ký tự và phải bao gồm ít nhất 1 ký tự đặc biệt.");
+                }                
+            }
+            else
+            {
+                MessageBox.Show("Đăng ký hoàn tất. Vui lòng đợi để chờ xác nhận !");
+            }
+
+
+
+        }
+
+        private void Username_texBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void TrangDangKy_Load(object sender, EventArgs e)
+        {
+            Username_texBox.Focus();
+        }
+    }
+}
